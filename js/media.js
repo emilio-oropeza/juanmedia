a5eaad87a18bd809ccc9da195cf986acfc434d29
AngelGlezz <angelglezz.7@gmail.com>
2016-04-27 13:38:19 -0500

    1
    wsdv

---
+++ js/media.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $('div.element a').click(function() {
+    var textoFiltro = $(this).text().toLowerCase().replace(/\s/g,"-");
+    if(textoFiltro == 'todos')
+    {
+        $('div.content').fadeIn('slow');
+    }
+    else
+    {
+        $('div.content').each(function()
+        {
+            if(!$(this).hasClass(textoFiltro))
+            {
+                $(this).fadeOut('normal').addClass('hidden');
+            }
+            else
+            {
+                $(this).fadeIn('slow').removeClass('hidden');
+            }
+        });
+    }
+    return false;
+    });
+});
